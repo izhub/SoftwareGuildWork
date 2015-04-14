16abb363ee6c44e7eb8716576acef5185ab87eda
Zahid Iqbal <zedinred@hotmail.com>
2015-04-14 13:29:37 -0400

    small change

--- NewFolder/GitTestNetBeans/src/main/java/com/swcguild/gittestnetbeans/HelloNetBeans.java
+++ NewFolder/GitTestNetBeans/src/main/java/com/swcguild/gittestnetbeans/HelloNetBeans.java
@@ -11,6 +11,6 @@ package com.swcguild.gittestnetbeans;
  */
 public class HelloNetBeans {
     public static void main (String[] args){
-        System.out.println("Hello, NetBeans");
+        System.out.println("Hello, Guild");
     }
 }
