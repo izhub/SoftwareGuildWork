27f95ddb24611d8cda1014e18f574297dd7bc31f
Zahid Iqbal <zedinred@hotmail.com>
2015-04-19 18:41:43 -0400

    step 3 done!

---
+++ Labs/RockPaperScissorsStep3/src/main/java/com/swcguild/rockpaperscissorsstep3/Step3.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swcguild.rockpaperscissorsstep3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author apprentice
+ */
+public class Step3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        int userPick = 0;
+        int pcPick = 0;
+        String strGame = "";
+        int rounds = 0;
+      
+        int ties = 0;
+        int uWin = 0;
+        int pcWin = 0;
+
+        do {
+            System.out.println("Would you like to play a game? " + "Y or N");
+            strGame = sc.next();
+        } while (!strGame.equalsIgnoreCase("y"));
+
+        do {
+            System.out.println("How many rounds would you like to play? ");
+            rounds = sc.nextInt();
+
+            if (rounds > 10 || rounds <= 0) {
+                System.out.println("Error! out of range.");
+                break;
+            }
+
+            for (int i = 1; i <= rounds; i++) {
+
+                System.out.println("\n Make your selection: \n 1. Rock \n 2. Paper \n 3. Scissor \n");
+                userPick = sc.nextInt();
+                pcPick = 1 + r.nextInt(3);
+
+                System.out.println("Computer choice was: " + pcPick);
+
+                if (userPick == pcPick) {
+                    System.out.println("It's a tie.");
+                    ties++;
+                } else if (userPick == 1 && pcPick == 2) {
+                    System.out.println("Computer win!.");
+                    pcWin++;
+                } else if (userPick == 1 && pcPick == 3) {
+                    System.out.println("You win. ");
+                    uWin++;
+                } else if (userPick == 2 && pcPick == 1) {
+                    System.out.println("You win!");
+                    uWin++;
+                } else if (userPick == 2 && pcPick == 3) {
+                    System.out.println("Computer wins.");
+                    pcWin++;
+                } else if (userPick == 3 && pcPick == 1) {
+                    System.out.println("Computer wins.");
+                    pcWin++;
+                } else if (userPick == 3 && pcPick == 2) {
+                    System.out.println("You win!");
+                    uWin++;
+                }
+
+            }
+            System.out.println("=============================");
+            System.out.println("Number of game ties: " + ties);
+            System.out.println("Number of computer wins: " + pcWin);
+            System.out.println("Number of your wins: " + uWin);
+            if (pcWin > uWin) {
+                System.out.println("Final winnner: Computer");
+
+            } else if (uWin > pcWin) {
+                System.out.println("Final winner: You!");
+            } else {
+                System.out.println("Game was a tie!");
+            }
+
+        } while (rounds > 10 || rounds <= 0);
+    }
+}
